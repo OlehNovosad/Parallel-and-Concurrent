88e3bccec980878a83c242557d7c730017c83049
Oleh Novosad <olehnovosad2013@gmail.com>
2026-03-03 00:30:09 +0000

    Add initial implementation for image downloading with sequential and parallel methods; configure CMake for project setup

---
+++ download_image/main.cpp
@@ -0,0 +1,108 @@
+#include <thread>
+#include <curl/curl.h>
+#include "boost/asio.hpp"
+
+/* declarations for download_image helper function */
+size_t download_image(int image_num);
+size_t write_callback(void* contents, size_t size, size_t nmemb, void* userp);
+
+/* sequential implementation of image downloader */
+size_t sequential_image_downloader(int num_images)
+{
+    size_t total_bytes = 0;
+    for (int i = 1; i <= num_images; i++)
+    {
+        total_bytes += download_image(i);
+        printf("Image #%d downloaded, total bytes so far: %d\n", i, static_cast<unsigned int>(total_bytes));
+    }
+    return total_bytes;
+}
+
+/* parallel implementation of image downloader */
+size_t parallel_image_downloader(const int num_images)
+{
+    std::atomic<size_t> total_bytes(0);
+    boost::asio::thread_pool thread_pool(30); // create a thread pool with 20 threads
+
+    for (int i = 0; i <= num_images; i++)
+    {
+        boost::asio::post(thread_pool, [i, &total_bytes] { total_bytes = download_image(i); });
+    }
+
+    thread_pool.join();
+
+    return total_bytes.load();
+}
+
+/* helper function to download a single image and return size in bytes */
+size_t download_image(int image_num)
+{
+    char url[] = "http://699340.youcanlearnit.net/imageXXX.jpg";
+    snprintf(url, sizeof(url), "http://699340.youcanlearnit.net/image%03d.jpg", ((image_num % 50) + 1));
+
+    curl_off_t num_bytes = 0;
+    if (CURL* curl = curl_easy_init())
+    {
+        curl_easy_setopt(curl, CURLOPT_URL, url);
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+        CURLcode res = curl_easy_perform(curl);
+        if (res != CURLE_OK)
+        {
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        }
+        res = curl_easy_getinfo(curl, CURLINFO_SIZE_DOWNLOAD_T, &num_bytes);
+        if (res != CURLE_OK)
+        {
+            fprintf(stderr, "curl_easy_getinfo() failed: %s\n", curl_easy_strerror(res));
+        }
+        curl_easy_cleanup(curl);
+    }
+    return num_bytes;
+}
+
+/* support function for the download_image helper function */
+size_t write_callback(void* contents, size_t size, size_t nmemb, void* userp)
+{
+    return size * nmemb;
+}
+
+int main()
+{
+    constexpr int NUM_EVAL_RUNS = 3;
+    constexpr int NUM_IMAGES = 50;
+
+    printf("Evaluating Sequential Implementation...\n");
+    std::chrono::duration<double> sequential_time(0);
+    size_t sequential_result = sequential_image_downloader(NUM_IMAGES);
+    for (int i = 0; i < NUM_EVAL_RUNS; i++)
+    {
+        auto startTime = std::chrono::high_resolution_clock::now();
+        sequential_image_downloader(NUM_IMAGES);
+        sequential_time += std::chrono::high_resolution_clock::now() - startTime;
+    }
+    sequential_time /= NUM_EVAL_RUNS;
+
+    printf("Evaluating Parallel Implementation...\n");
+    std::chrono::duration<double> parallel_time(0);
+    size_t parallel_result = parallel_image_downloader(NUM_IMAGES);
+    for (int i = 0; i < NUM_EVAL_RUNS; i++)
+    {
+        auto startTime = std::chrono::high_resolution_clock::now();
+        parallel_image_downloader(NUM_IMAGES);
+        parallel_time += std::chrono::high_resolution_clock::now() - startTime;
+    }
+    parallel_time /= NUM_EVAL_RUNS;
+
+    // display sequential and parallel results for comparison
+    if (sequential_result != parallel_result)
+    {
+        printf("ERROR: Result mismatch!\n  Sequential Result = %zd\n  Parallel Result = %zd\n", sequential_result,
+               parallel_result);
+    }
+    printf("Average Sequential Time: %.2f ms\n", sequential_time.count() * 1000);
+    printf("  Average Parallel Time: %.2f ms\n", parallel_time.count() * 1000);
+    printf("Speedup: %.2f\n", sequential_time / parallel_time);
+    printf("Efficiency %.2f%%\n", 100 * (sequential_time / parallel_time) / std::thread::hardware_concurrency());
+
+    return 0;
+}
